2a105adf32c2aaedcc0f8e625adabcf8bf8175af
olehtheengineer <olehtheengineer@gmail.com>
2026-02-19 16:58:32 +0200

    CICD epic integration

--- src/core/middleware.py
+++ src/core/middleware.py
@@ -1,35 +1,32 @@
-from django.http import HttpResponseRedirect, HttpResponse
-from django.conf import settings
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-
 __all__ = [
-    'is_restricted_internal_url',
-    'login_required_middleware',
+    "is_restricted_internal_url",
+    "login_required_middleware",
 ]
 
 
 def is_restricted_internal_url(url):
-    URL_PREFIXES_EXCLUDES = [
+    url_prefixes_excludes = [
         # '/media/',
-        '/__debug__/',
-        '/login/',
-        '/register/',
-        '/logout/',
-        '/password-',
-        '/reset/',
-        '/superadmin/',
+        "/__debug__/",
+        "/login/",
+        "/register/",
+        "/logout/",
+        "/password-",
+        "/reset/",
+        "/superadmin/",
     ]
-    return not max([url.startswith(x) for x in URL_PREFIXES_EXCLUDES])
+    return not any(url.startswith(x) for x in url_prefixes_excludes)
 
 
 def login_required_middleware(get_response):
     def middleware(request):
-        assert hasattr(request, 'user')
-        if not request.user.is_authenticated:
-            if is_restricted_internal_url(request.path_info):
-                return HttpResponseRedirect(reverse('login'))
+        assert hasattr(request, "user")
+        if not request.user.is_authenticated and is_restricted_internal_url(request.path_info):
+            return HttpResponseRedirect(reverse("login"))
 
-        response = get_response(request)
-        return response
+        return get_response(request)
+
     return middleware
